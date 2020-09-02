7ada2ee43ed45c1dda5e40f02208c84988d07199
oumq <417585271@qq.com>
2020-09-02 11:29:08 +0800

    test(): :construction: pwa test

--- vue.config.js
+++ vue.config.js
@@ -46,8 +46,9 @@ module.exports = {
   },
   pwa: {
     workboxOptions: {
-      // skipWaiting: true,
-      // clientsClaim: true
+      importWorkboxFrom: 'local',
+      skipWaiting: true,
+      clientsClaim: true
     }
   }
 }
