8519379eada55c57431c907265d899bb354db528
oumq <417585271@qq.com>
2020-09-04 00:11:16 +0800

    perf(): :wrench: 修改最小压缩体积

--- vue.config.js
+++ vue.config.js
@@ -56,7 +56,7 @@ module.exports = {
               test: new RegExp(
                 '\\.(' + productionGzipExtensions.join('|') + ')$'
               ), // 匹配文件名
-              threshold: 10240, // 对10K以上的数据进行压缩
+              threshold: 1024, // 对1K以上的数据进行压缩
               minRatio: 0.8,
               deleteOriginalAssets: false // 是否删除源文件
             })
