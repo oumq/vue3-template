3fc5bafcf0b5986d74e32c0012a5099fed395917
oumq <417585271@qq.com>
2021-01-20 15:14:38 +0800

    test(): 打包测试

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}']
-  },
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {}
